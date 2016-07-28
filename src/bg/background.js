4033931e344c3b40451f784c3820ff2fc2f7df3b
Matthew Salcido <matthew.salcido@healthsparq.com>
2016-07-28 14:09:35 -0700

    Bump to 1.0.2

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,5 +1,6 @@
 let initElems = [];
 
+// Inject the css/js files into the DOM
 function appendFragment(source) {
 
   let fragment = document.createDocumentFragment();
@@ -12,10 +13,12 @@ function appendFragment(source) {
   (document.head || document.documentElement).appendChild(fragment.cloneNode(true));
 }
 
+// Check for last saved preference
 chrome.storage.sync.get('sanitize', function(result) {
 
   let inject;
 
+  // create the object if it does not yet exist
   if (!result) {
 
     chrome.storage.sync.set({sanitize: true});
@@ -25,13 +28,9 @@ chrome.storage.sync.get('sanitize', function(result) {
 
   // Create injectCss element...
   inject = document.createElement('link');
-
   inject.rel = 'stylesheet';
-
   inject.type = 'text/css';
-
   inject.href = chrome.extension.getURL('src/inject/inject.css');
-
   inject.id = 'injectCss';
 
   // disable if needed
